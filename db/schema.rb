4c980eae00eabd7f05e397a3098363dc09e79a0d
Aaron Soules <asoules@gmail.com>
2014-06-08 12:36:18 -0400

    postgres in dev

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140607235707) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "agencies", force: true do |t|
     t.float    "latitude"
     t.float    "longitude"
