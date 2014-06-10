c992c7fd07c789110d62cf7d7c23e070faa4462d
Patrick Brown <patrick@gravityswitch.com>
2014-06-10 19:46:13 -0400

    add is_open class method with tests

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -9,4 +9,46 @@ class Agency < ActiveRecord::Base
     [address, city, state].join(', ')
   end
 
+  def is_open(time)
+    day_of_week = time.strftime('%A')
+    am_pm = time.strftime('%p')
+    
+    if am_pm == 'AM'
+      if day_of_week == "Monday"
+        self.open_mon_am
+      elsif day_of_week == "Tuesday"
+        self.open_tue_am
+      elsif day_of_week == "Wednesday"
+        self.open_wed_am
+      elsif day_of_week == "Thursday"
+        self.open_thr_am
+      elsif day_of_week == "Friday"
+        self.open_fri_am
+      elsif day_of_week == "Saturday"
+        self.open_sat_am
+      elsif day_of_week == "Sunday"
+        self.open_sun_am
+      else
+        false
+      end
+    else
+      if day_of_week == "Monday"
+        self.open_mon_pm
+      elsif day_of_week == "Tuesday"
+        self.open_tue_pm
+      elsif day_of_week == "Wednesday"
+        self.open_wed_pm
+      elsif day_of_week == "Thursday"
+        self.open_thr_pm
+      elsif day_of_week == "Friday"
+        self.open_fri_pm
+      elsif day_of_week == "Saturday"
+        self.open_sat_pm
+      elsif day_of_week == "Sunday"
+        self.open_sun_pm
+      else
+        false
+      end
+    end
+  end
 end
